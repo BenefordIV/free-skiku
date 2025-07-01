26f24a087f5756a4c5b99fe7ebf4c3431a12e3d5
Ben Steier <bensteier713@gmail.com>
2025-07-01 16:48:16 -0500

    updated logic to add in ski_lifts after x distance (currently 1000 px). Updated logic to queue_free once the ski lift has made it through a barrier. Updated spawn location for ski_lift

--- chair_lift.gd
+++ chair_lift.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-@onready var chair_up: Area2D = $chair_up
-@onready var chair_down: Area2D = $chair_down
+class_name CHAIR_LIFT_ANIM
+
+@onready var chair_up: StaticBody2D = $chair_up
+@onready var chair_down: StaticBody2D = $chair_down
 
 const _y_movement: float = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	SignalHub.on_ski_lift_despawn.connect(despawn_ski_lift)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	chair_up.position.y -= _y_movement * delta
-	chair_down.position.y = _y_movement * delta
+	chair_down.position.y += _y_movement * delta
+
+func despawn_ski_lift() -> void:
+	queue_free()
